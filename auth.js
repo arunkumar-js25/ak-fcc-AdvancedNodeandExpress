fb1dbe03f99dfaf9bb387e5fa33d9e49260beffc
Arun Kumar <53009824+arunkumar-js25@users.noreply.github.com>
2022-11-09 07:19:00 +0000

    Github Outh Token for users - completed

--- auth.js
+++ auth.js
@@ -1,9 +1,23 @@
+require('dotenv').config();
 const { ObjectID } = require('mongodb');
 const LocalStrategy = require('passport-local');
 const passport = require('passport');
 const bcrypt = require('bcrypt');
+const GitHubStrategy = require('passport-github').Strategy;
 
 module.exports = function (app, myDataBase) {
+
+  // Serialization and deserialization here...
+  passport.serializeUser((user, done) => {
+    done(null, user._id);
+  });
+
+  passport.deserializeUser((id, done) => {
+    myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
+      done(null, doc);
+    });
+  });
+  
   passport.use(new LocalStrategy((username, password, done) => {
   myDataBase.findOne({ username: username }, (err, user) => {
       console.log(`User ${username} attempted to log in.`);
@@ -18,14 +32,36 @@ module.exports = function (app, myDataBase) {
     });
   }));
 
-  // Serialization and deserialization here...
-  passport.serializeUser((user, done) => {
-    done(null, user._id);
-  });
-
-  passport.deserializeUser((id, done) => {
-    myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
-      done(null, doc);
-    });
-  });
+  passport.use(new GitHubStrategy({
+  clientID: process.env.GITHUB_CLIENT_ID,
+  clientSecret: process.env.GITHUB_CLIENT_SECRET,
+  callbackURL: 'https://ak-fcc-AdvancedNodeandExpress.arunkumar-js25.repl.co/auth/github/callback'
+},
+  function(accessToken, refreshToken, profile, cb) {
+    console.log(profile);
+    //Database logic here with callback containing your user object
+    myDataBase.findAndModify(
+        { id: profile.id },
+        {},
+        {
+          $setOnInsert: {
+            id: profile.id,
+            username: profile.displayName || 'John Doe',
+            photo: profile.photos[0].value || '',
+            email: Array.isArray(profile.emails) ? profile.emails[0].value : 'No public email',
+            created_on: new Date(),
+            provider: profile.provider || ''
+          }, $set: {
+            last_login: new Date()
+          }, $inc: {
+            login_count: 1
+          }
+        },
+        { upsert: true, new: true },
+        (err, doc) => {
+          return cb(null, doc.value);
+        }
+      );
+  }
+));
 }
--- routes.js
+++ routes.js
@@ -17,10 +17,16 @@ module.exports = function (app, myDataBase) {
       title: 'Hello',
       message: 'Please login',
       showLogin: true,
-      showRegistration: true
+      showRegistration: true,
+      showSocialAuth: true
     });
   });
 
+  app.route('/auth/github').get(passport.authenticate('github'));
+  app.route('/auth/github/callback').get(passport.authenticate('github', { failureRedirect: '/' }), (req, res) => {
+    res.redirect('/profile');
+  })
+  
   app.route('/login').post(passport.authenticate('local', { failureRedirect: '/' }), (req, res) => {
     res.redirect('/profile');
   })
